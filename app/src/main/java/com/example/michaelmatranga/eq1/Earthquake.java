33c412d68c712caeed17fce852679c73e3a50370
Michael Matranga <mamatranga@gmail.com>
2017-07-21 13:06:57 -0400

    Added basic model object.

---
+++ app/src/main/java/com/example/michaelmatranga/eq1/Earthquake.java
@@ -0,0 +1,10 @@
+package com.example.michaelmatranga.eq1;
+
+public class Earthquake {
+    private String mDetail;
+    private Double mLatitude;
+    private Double mLongitude;
+    private Double mTimeInSeconds;
+    private String mMagnitude;
+    private String mPlace;
+}
