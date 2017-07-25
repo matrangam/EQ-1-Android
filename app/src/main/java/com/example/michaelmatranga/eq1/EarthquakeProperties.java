3cc0c43660a508926ef1cd2a58e42ab7353f275b
Michael Matranga <mamatranga@gmail.com>
2017-07-25 11:36:57 -0400

    Getting all useful properties.

--- app/src/main/java/com/example/michaelmatranga/eq1/EarthquakeProperties.java
+++ app/src/main/java/com/example/michaelmatranga/eq1/EarthquakeProperties.java
@@ -3,24 +3,44 @@ package com.example.michaelmatranga.eq1;
 import com.google.gson.annotations.SerializedName;
 
 import java.math.BigInteger;
+import java.util.List;
 
 class EarthquakeProperties {
 
     @SerializedName("properties")
     private Earthquake properties;
 
-    public class Earthquake {
+    class Earthquake {
         @SerializedName("detail")
         private String mDetail;
-        private Double mLatitude;
-        private Double mLongitude;
+
         @SerializedName("time")
         private BigInteger mTimeInSeconds;
+
         @SerializedName("mag")
         private Double mMagnitude;
+
         @SerializedName("place")
         private String mPlace;
+
         @SerializedName("title")
-        private String title;
+        private String mTitle;
+
+    }
+
+    @SerializedName("geometry")
+    private Geometry mGeometry;
+
+    class Geometry {
+        @SerializedName("mCoordinates")
+        private List<Double> mCoordinates;
+    }
+
+    public Double getLatitude() {
+        return mGeometry.mCoordinates.get(0);
+    }
+
+    public Double getLongitude() {
+        return mGeometry.mCoordinates.get(1);
     }
 }
